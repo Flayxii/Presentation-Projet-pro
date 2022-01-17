9a52b45c7221ce5045ac442ebe705532627945ba
ma-j <mj@itc-rh.fr>
2022-01-17 15:08:47 +0100

    ererererre

--- app.js
+++ app.js
@@ -36,6 +36,32 @@ window.onclick = function(event) {
   }
 }
 
+
+// Modal2
+
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myDesktop");
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+  }
+
 // Slider
 
 const imgs = document.querySelectorAll('.cont-slides img');

1ee3652c92a4980f082f2beee01e20855e6c7de1
ma-j <mj@itc-rh.fr>
2022-01-25 15:29:14 +0100

    ajou

--- app.js
+++ app.js
@@ -41,21 +41,21 @@ window.onclick = function(event) {
 
 var modal2 = document.getElementById("myModal2");
 
-// Get the button that opens the modal
+// bouton
 var btn2 = document.getElementById("myDesktop");
 var span2 = document.getElementsByClassName("close")[1];
 
-// When the user clicks on the button, open the modal
+// Click bouton ouvre modal
 btn2.onclick = function() {
     modal2.style.display = "block";
   }
   
-  // When the user clicks on <span> (x), close the modal
+  // Click (x), ferme modal
   span2.onclick = function() {
     modal2.style.display = "none";
   }
   
-  // When the user clicks anywhere outside of the modal, close it
+  // click out ferme modal
   window.onclick = function(event) {
     if (event.target == modal2) {
       modal2.style.display = "none";
